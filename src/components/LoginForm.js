e6df8771767a68605d8a86d131db6e02317cb1e4
nishat-arif <nishatarif1912@gmail.com>
2025-07-30 10:31:03 -0400

    user signuped on firebase

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,11 @@
 import { useRef, useState } from "react"
 import {validateLoginForm} from "../utils/validate"
+import { createUserWithEmailAndPassword , signInWithEmailAndPassword} from "firebase/auth";
+import { auth } from "../utils/firebase";
 
 const LoginForm =()=>{
 
     const [isSignInForm , setIsSignInForm] = useState(true)
-
     const [errorMessage , setErrorMessage] = useState(null)
 
     const email = useRef(null)
@@ -19,9 +20,42 @@ const LoginForm =()=>{
     }
 
     const handleBtnClick =()=>{
-
         const message = validateLoginForm(email.current.value, password.current.value)
         setErrorMessage(message)
+
+        //create new user(sign/signup successfull) create a new user in firebase
+        if(message) return; // if there is errormessage return from here and dont proceed further.
+
+        if(!isSignInForm){
+            //signup 
+            createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    console.log("user", user)
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    //setErrorMessage(errorCode + errorMessage)
+
+                });
+
+        }
+        else{
+            //signin
+            signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    console.log("user", user)
+                    
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    //setErrorMessage(errorCode + errorMessage)
+                });
+
+        }
 
     }
 
@@ -29,8 +63,8 @@ const LoginForm =()=>{
     <div className="absolute w-3/12 mx-auto right-0 left-0  my-44  ">
         <form onSubmit={(e)=>{e.preventDefault()}} className=" p-12  bg-black text-white rounded-lg bg-opacity-80">
             <h1 className="font-bold text-3xl py-4">{isSignInForm ?'Sign In' : 'Sign Up'}</h1>
-            {!isSignInForm && <input type="text" placeholder="Full Name" className="p-2 my-2 w-full bg-gray-700"></input>}
             {(errorMessage != null) && <p className="bg-yellow-600 text-black my-2 p-2">{errorMessage}</p>}
+            {!isSignInForm && <input type="text" placeholder="Full Name" className="p-2 my-2 w-full bg-gray-700"></input>}
             <input ref ={email} type="text" placeholder="Email address" className="p-2 my-2 w-full bg-gray-700"></input>
             <input ref={password} type="text" placeholder="Password" className="p-2 my-2 w-full bg-gray-700"></input>
             <button className="p-2 my-4 bg-red-800 w-full rounded-lg cursor-pointer" onClick={handleBtnClick}>{isSignInForm ?'Sign In' : 'Sign Up'}</button>
